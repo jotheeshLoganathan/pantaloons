39ad31528edeca51ed67e340a00ff6ea76ae4317
jotheeshLoganathan <cadbary676@gmail.com>
2024-08-24 16:43:19 +0530

    24-08-24

--- src/collection/Trend.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Trend = () => {
-  return (
-    <div>Trend</div>
-  )
-}
-
-export default Trend
--- src/store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import Cardslice from "./cardslice/Cardslice";
-
-export const store = configureStore({
-    reducer : {
-        cart : Cardslice
-    },
-    devTools : true
-})
