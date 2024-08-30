3e0f52ca48ee4b6d70f8e89f9ead28806985f220
jotheeshLoganathan <cadbary676@gmail.com>
2024-08-30 11:00:34 +0530

    08-30-2024 validation

--- project/src/companent/Form.js
+++ project/src/companent/Form.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import"./form.css"
 
 const LoginForm = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+    const [errors, setErrors] = useState({});
+
+    const validate =() =>{
+        const NewErrors = {};
+        if (!formData.username) NewErrors.username = 'Username is required';
+        if (!formData.email) NewErrors.email = 'Email is required';
+        else if (!/\S+@\S+\.\S+/.test(formData.email)) NewErrors.email = 'Email is invalid';
+
+        if (!formData.password) NewErrors.password = 'Password is required';
+        else if(formData.password.length < 8) NewErrors.password = 'Password must be at least 8 chateres';
+        else if (!/[a-z]/.test(formData.password)) NewErrors.password = 'Password must contain at least one lowercase';
+        else if (!/[A-Z]/.test(formData.password)) NewErrors.password = 'Password must contain at least one uppercase';
+        else if (!/[!@#$%^&*(){},.?><"]/.test(formData.password)) NewErrors.password = 'Password must contain at least one special characters';
+
+        return NewErrors;
+    };
+    const handelChange = (e) =>{
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) =>{
+        e.preventDefault ();
+        const valedaitonErrors = validate();
+        if (Object.keys(valedaitonErrors).length === 0) {
+            console.log('form data is valid:', formData);
+
+            setFormData({
+                username:'',
+                email:'',
+                password:''
+            });
+            setErrors({});
+        } else {
+            setErrors(valedaitonErrors);
+        }
+    };
   return (
     <div className='form-background'>
         <div className='row'>
@@ -10,22 +55,40 @@ const LoginForm = () => {
                 <div className='form-valedaiton card'>
                     <h1 className='heading-for-form'>Register</h1>
                 <div class="card-body">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div class="mb-3">
                             <label for="exampleInputUserName"className='form-label'>User Name</label>
-                            <input type='text' className='form-control' id='exampleInputUserName'/>
+                            <input 
+                            type='text'
+                            name='username'
+                            value={formData.username}
+                            onChange={handelChange}
+                             className='form-control' 
+                             id='exampleInputUserName'/>
+                             {errors.username && <p className='alert errors alert-danger'>{errors.username}</p>}
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                            <input 
+                            type="email"
+                            name='email'
+                            value={formData.email}
+                            onChange={handelChange} 
+                            class="form-control" 
+                            id="exampleInputEmail1" 
+                            aria-describedby="emailHelp"/>
+                            {errors.email && <p className='alert errors alert-danger'>{errors.email}</p>}
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1"/>
-                        </div>
-                        <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            <input 
+                            type="password"
+                            name='password'
+                            value={formData.password}
+                            onChange={handelChange} 
+                            class="form-control" 
+                            id="exampleInputPassword1"/>
+                            {errors.password && <p className='alert errors alert-danger'>{errors.password}</p>}
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
@@ -33,7 +96,6 @@ const LoginForm = () => {
                 </div>
             </div>
             <div className='col-md-4'></div>
-
         </div>
     </div>
 
